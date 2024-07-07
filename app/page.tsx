a17b085af62a8e42380a19b424b8bf874a514d64
raymond <raymond.luke.spartz@gmail.com>
2024-07-07 17:55:33 -0500

    Add Link

--- app/page.tsx
+++ app/page.tsx
@@ -7,6 +7,7 @@ export default function Home() {
         <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onclick-move-object">OnClick Move Object</Link>
         <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onload-text-change">OnLoad Text Change</Link>
         <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onmousedown-open-modal">OnMouseDown Open Modal</Link>
+        <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onload-move-camera">OnLoad Move Camera</Link>
       </div>
     </main>
   );
