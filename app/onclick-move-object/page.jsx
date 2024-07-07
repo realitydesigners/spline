a3dce325d52938ad608718ed5c9eeac553899407
raymond <raymond.luke.spartz@gmail.com>
2024-07-07 17:06:10 -0500

    + add home page navigation

--- app/onclick-move-object/page.jsx
+++ app/onclick-move-object/page.jsx
@@ -11,7 +11,6 @@ const OnLoadTextChange = () => {
 		const Object = spline.findObjectByName("MoveThis");
 		if (ObjectRef) {
 			ObjectRef.current = Object;
-
 		}
 	};
 
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,13 @@
-
+import Link from "next/link"
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-
+    <main className="flex min-h-screen flex-col items-center justify-center">
+      <div className="flex flex-col gap-2 font-mono text-sm">
+        <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onclick-move-object">OnClick Move Object</Link>
+        <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onload-text-change">OnLoad Text Change</Link>
+        <Link className="text-white px-4 py-2 hover:bg-gray-600 border border-gray-600 rounded-xl" href="/onmousedown-open-modal">OnMouseDown Open Modal</Link>
+      </div>
     </main>
   );
 }
